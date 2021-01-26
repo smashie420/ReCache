ab2662d62af6aa59665aa6154b588b58dabf0755
DEVIN G <devingomez1@hotmail.com>
2021-01-26 10:51:41 -0800

    Finished Commit

---
+++ Discord Cache Backup/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace Discord_Cache_Backup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(68)))), ((int)(((byte)(75)))));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(188, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(332, 72);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "BACKUP";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(68)))), ((int)(((byte)(75)))));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Location = new System.Drawing.Point(188, 135);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(332, 72);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "RESTORE";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(43)))), ((int)(((byte)(47)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Location = new System.Drawing.Point(1, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(686, 50);
+            this.panel1.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Discord_Cache_Backup.Properties.Resources.ultra_high_res_logo;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button3.Image = global::Discord_Cache_Backup.Properties.Resources.icons8_delete_50;
+            this.button3.Location = new System.Drawing.Point(632, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 50);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(57)))), ((int)(((byte)(63)))));
+            this.progressBar1.ForeColor = System.Drawing.Color.Black;
+            this.progressBar1.Location = new System.Drawing.Point(188, 226);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(332, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Malgun Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(46, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 37);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "SG ReCache";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(57)))), ((int)(((byte)(63)))));
+            this.ClientSize = new System.Drawing.Size(684, 261);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Discord Cache Backup";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Discord Cache Backup/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace Discord_Cache_Backup
+{
+    public partial class Form1 : Form
+    {
+
+        public class UsrData
+        {
+            public string[] localStorageSplit { get; set; }
+            public string localStorageLocation { get; set; }
+
+            public string[] sessionStorageSplit { get; set; }
+            public string sessionStorageLocation { get; set; }
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+
+            if (!File.Exists("backup"))
+            {
+                Directory.CreateDirectory("backup");
+            }
+            if(!File.Exists("backup/Local Storage"))
+            {
+                Directory.CreateDirectory("backup/Local Storage");
+            }
+            if (!File.Exists("backup/Session Storage"))
+            {
+                Directory.CreateDirectory("backup/Session Storage");
+            }
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.FlatAppearance.BorderSize = 0;
+
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.FlatAppearance.BorderSize = 0;
+
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.FlatAppearance.BorderSize = 0;
+
+            /*
+            string[] files = Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\discord\\Local Storage\\leveldb", "*.log", SearchOption.AllDirectories); // Array: ["C:\\Users\\smashie420\\AppData\\Roaming\\discord\\Local Storage\\leveldb\\00003.log"]
+            UsrData data = new UsrData
+            {
+                fileDir = String.Join("", files), // String : "C:\\Users\\smashie420\\AppData\\Roaming\\discord\\Local Storage\\leveldb\\00003.log"
+                fileSplit = String.Join("", files).Split('\\'), // Array: ["C:","Users","Smashie420","AppData", "Roaming", "discord", "Local Storage", "leveldb", "00003.log"]
+                fileName = String.Join("", files).Split('\\')[String.Join("", files).Split('\\').Length - 1] // String: 00003.log
+            };
+
+            
+            string[] files = Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\discord\\Local Storage\\leveldb", "*.log", SearchOption.AllDirectories); // Array: ["C:\\Users\\smashie420\\AppData\\Roaming\\discord\\Local Storage\\leveldb\\00003.log"]
+            fileDir = String.Join("", files); // String : "C:\\Users\\smashie420\\AppData\\Roaming\\discord\\Local Storage\\leveldb\\00003.log"
+            fileSplit = fileDir.Split('\\'); // Array: ["C:","Users","Smashie420","AppData", "Roaming", "discord", "Local Storage", "leveldb", "00003.log"]
+            fileName = fileSplit[fileSplit.Length - 1]; // String: 00003.log
+            */
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case 0x84:
+                    base.WndProc(ref m);
+                    if ((int)m.Result == 0x1)
+                        m.Result = (IntPtr)0x2;
+                    return;
+            }
+
+            base.WndProc(ref m);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UsrData data = new UsrData
+            {
+                localStorageSplit = String.Join("", Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\discord\\Local Storage").Split('\\'), // Array: ["C:","Users","Smashie420","AppData", "Roaming", "discord", "Local Storage", "leveldb", "00003.log"]
+                localStorageLocation = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\discord\\Local Storage",
+                sessionStorageSplit = String.Join("", Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\discord\\Session Storage").Split('\\'), // Array: ["C:","Users","Smashie420","AppData", "Roaming", "discord", "Local Storage", "leveldb", "00003.log"]
+                sessionStorageLocation = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\discord\\Session Storage"
+            };
+            File.WriteAllText("data.json", JsonConvert.SerializeObject(data));
+            progressBar1.Value = 10;
+            // Local storage stuff
+            foreach (string dirPath in Directory.GetDirectories(data.localStorageLocation, "*", SearchOption.AllDirectories))
+                Directory.CreateDirectory(dirPath.Replace(data.localStorageLocation, "backup/Local Storage/"));
+            progressBar1.Value = 30;
+            //Copy all the files & Replaces any files with the same name
+            foreach (string newPath in Directory.GetFiles(data.localStorageLocation, "*.*", SearchOption.AllDirectories))
+                File.Copy(newPath, newPath.Replace(data.localStorageLocation, "backup/Local Storage/"), true);
+            progressBar1.Value = 40;
+            //MessageBox.Show("Finished backing up data!", "OK", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+
+            // Session Storage Stuff
+            foreach (string dirPath in Directory.GetDirectories(data.sessionStorageLocation, "*", SearchOption.AllDirectories))
+                Directory.CreateDirectory(dirPath.Replace(data.sessionStorageLocation, "backup/Session Storage/"));
+            progressBar1.Value = 80;
+            //Copy all the files & Replaces any files with the same name
+            foreach (string newPath in Directory.GetFiles(data.sessionStorageLocation, "*.*", SearchOption.AllDirectories))
+                File.Copy(newPath, newPath.Replace(data.sessionStorageLocation, "backup/Session Storage/"), true);
+
+            progressBar1.Value = 100;
+            MessageBox.Show("Finished backing up data!", "OK", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        
+        
+    private void button2_Click(object sender, EventArgs e)
+        {
+            
+            if (Process.GetProcessesByName("Discord").Length > 0)
+            {
+                MessageBox.Show("Discord is running please close it");
+                return;
+            }
+            string json = File.ReadAllText("data.json");
+            UsrData data = JsonConvert.DeserializeObject<UsrData>(json);
+            progressBar1.Value = 10;
+            foreach (string dirPath in Directory.GetDirectories("backup/Local Storage/", "*", SearchOption.AllDirectories))
+                Directory.CreateDirectory(dirPath.Replace("backup/Local Storage/", data.localStorageLocation+"\\"));
+            progressBar1.Value = 30;
+            //Copy all the files & Replaces any files with the same name
+            foreach (string newPath in Directory.GetFiles("backup/Local Storage/", "*.*", SearchOption.AllDirectories))
+                File.Copy(newPath, newPath.Replace("backup/Local Storage/", data.localStorageLocation + "\\"), true);
+            progressBar1.Value = 60;
+            /* Dont need this because Session storage doesnt have any sub-directories
+             foreach (string dirPath in Directory.GetDirectories("backup/Session Storage/", "*", SearchOption.AllDirectories))
+                 Directory.CreateDirectory(dirPath.Replace("backup/Session Storage/", data.sessionStorageLocation)); //data.sessionStorageLocation, "backup/Session Storage/"
+             */
+
+            //Copy all the files & Replaces any files with the same name
+            foreach (string newPath in Directory.GetFiles("backup/Session Storage/", "*.*", SearchOption.AllDirectories))
+                File.Copy(newPath, newPath.Replace("backup/Session Storage/", data.sessionStorageLocation), true);
+            progressBar1.Value = 100;
+            MessageBox.Show("Finished restoring up data!", "OK", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
